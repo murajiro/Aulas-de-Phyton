109a726548e3ee475fa4347f6dcdf3b654abd909
Enzo Murajiro <104034917+murajiro@users.noreply.github.com>
2024-08-16 10:00:09 -0300

    Add files via upload

---
+++ Media de avs.py
@@ -0,0 +1,5 @@
+nome=input("Digite o nome do aluno: ")
+av1=float(input("Digite a nota da AV1: "))
+av2=float(input("Digite a nota da AV2: "))
+media=(av1+av2)/2
+print(f"Essas são as notas e a média do(a) aluno(a): {nome} \n AV1:{av1}\n AV2: {av2}\n Média: {media:.2f}")
